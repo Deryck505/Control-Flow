cad26c27ec77703b93c240a26a670f90b2fc36c6
Derick <derickmjema505@gmail.com>
2024-03-05 17:47:45 +0300

    Frst Commit

---
+++ week4.dart
@@ -0,0 +1,62 @@
+class machine {
+  String? name;
+  int? price;
+  num? weight;
+
+  void start() {
+    print("Machine is started");
+  }
+
+  void stop() {
+    print("Machine has stopped");
+  }
+}
+
+//Inheritance
+abstract class laptop implements machine {
+  String? version;
+  @override
+  void start() {
+    print("Laptop is started");
+  }
+
+  void stop() {
+    print("Laptop is stopped");
+  }
+}
+
+// Interphase
+class makbook extends machine {
+  String? model;
+  String? color;
+
+  void showDetails() {
+    print("The name: $name");
+    print("Buying price: $price");
+    print("Weight: $weight");
+    print("color: $color");
+    print("model: $model");
+  }
+
+  @override
+  void start() {
+    print("Macbook is on");
+  }
+
+  void stop() {
+    print("Mac stopping");
+  }
+}
+
+void main() {
+  makbook m1 = new makbook();
+  m1.name = "macbkkok pro";
+  m1.price = 2000000; //ksh
+  m1.weight = 2000; //grams
+  m1.model = "2019v2";
+  m1.color = "black";
+
+  m1.showDetails();
+  m1.start();
+  m1.stop();
+}
